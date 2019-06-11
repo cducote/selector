b5777c72dd10c6185eb524a3d8fd4c07822fc636
cducote <chrisducote89@gmail.com>
2019-06-11 16:26:45 -0400

    scaling on point

--- src/Components/UnitOverlay.js
+++ src/Components/UnitOverlay.js
@@ -5,16 +5,16 @@ import ImageMapper from 'react-image-mapper'
 var MAP = {
 	name: 'my-map',
 	areas: [
-		{name: '1', shape: 'rect', coords: [25,33,27,300], preFillColor: 'green', fillColor: 'blue'},
-		{name: '2', shape: 'rect', coords: [219,118,220,210], preFillColor: 'pink'},
-		{name: '3', shape: 'rect', coords: [381,241,383,94], fillColor: 'yellow'},
-		{name: '4', shape: 'rect', coords: [245,285,290,285], preFillColor: 'red'},
-        {name: '5', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
-        {name: '6', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
-        {name: '7', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
-        {name: '8', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
-        {name: '9', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
-        {name: '10', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'}
+		{name: '1', shape: 'rect', coords: [564,153,352,10], preFillColor: 'green'},
+		{name: '2', shape: 'rect', coords: [769,14,978,149], preFillColor: 'pink'},
+		{name: '3', shape: 'rect', coords: [1154,15,1371,151], prefillColor: 'green'},
+		// {name: '4', shape: 'rect', coords: [245,285,290,285], preFillColor: 'red'},
+        // {name: '5', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
+        // {name: '6', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
+        // {name: '7', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
+        // {name: '8', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
+        // {name: '9', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'},
+        // {name: '10', shape: 'rect', coords: [170,100,200,50], fillColor: 'orange'}
 	]
 };
 
@@ -22,7 +22,8 @@ class UnitOverlay extends Component {
     render() {
         return (
         <div className='unitOverlay'>
-            <ImageMapper src={unit} width={1250} map={MAP}/>
+            <ImageMapper src={unit} width={1440} imgWidth={1920} map={MAP}/>
+            {/* somehow this works */}
             
         </div>
         );
