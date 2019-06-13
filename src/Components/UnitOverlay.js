4ace32e40bc0fcf455dac93e43199d852493eaac
cducote <chrisducote89@gmail.com>
2019-06-13 18:26:36 -0400

    cleaned up working modal

--- src/Components/UnitOverlay.js
+++ src/Components/UnitOverlay.js
@@ -8,37 +8,27 @@ let MAP = {
 	id: 'map', name: 'my-map',
 	areas: [
 		{id: 1, shape: 'rect', coords: [564,153,352,10], preFillColor: 'clear'},
-		{id: 2, shape: 'rect', coords: [769,14,980,150], preFillColor: 'pink'},
-		{id: 3, shape: 'rect', coords: [1154,10,1371,151], preFillColor: 'yellow'},
-		{id: 4, shape: 'rect', coords: [320,368,535,509], preFillColor: 'red'},
-        {id: 5, shape: 'rect', coords: [1387,307,1600,443], preFillColor: 'orange'},
-        {id: 6, shape: 'rect', coords: [367,997,584,1136], preFillColor: 'teal'},
-        {id: 7, shape: 'rect', coords: [804,1001,1019,1135], preFillColor: 'purple'},
-        {id: 8, shape: 'rect', coords: [1254,1001,1466,1136], preFillColor: 'lightgreen'},
-        {id: 9, shape: 'rect', coords: [516,1294,732,1428], preFillColor: 'darkred'},
-        {id: 10, shape: 'rect', coords: [1034,1292,1246,1428], preFillColor: 'blue'}
+		{id: 2, shape: 'rect', coords: [769,14,980,150], preFillColor: 'clear'},
+		{id: 3, shape: 'rect', coords: [1154,10,1371,151], preFillColor: 'clear'},
+		{id: 4, shape: 'rect', coords: [320,368,535,509], preFillColor: 'clear'},
+        {id: 5, shape: 'rect', coords: [1387,307,1600,443], preFillColor: 'clear'},
+        {id: 6, shape: 'rect', coords: [367,997,584,1136], preFillColor: 'clear'},
+        {id: 7, shape: 'rect', coords: [804,1001,1019,1135], preFillColor: 'clear'},
+        {id: 8, shape: 'rect', coords: [1254,1001,1466,1136], preFillColor: 'clear'},
+        {id: 9, shape: 'rect', coords: [516,1294,732,1428], preFillColor: 'clear'},
+        {id: 10, shape: 'rect', coords: [1034,1292,1246,1428], preFillColor: 'clear'}
 	]
 };
 
 class UnitOverlay extends Component {
     constructor(props, context) {
         super(props, context);
-    
         this.handleShow = this.handleShow.bind(this);
         this.handleClose = this.handleClose.bind(this);
-    
         this.state = {
           show: false,
         };
       }
-    
-    // state = {
-    //     buttons: {
-    //         id: [1,2,3,4,5,6,7,8,9,10]
-    //     },
-    //     show: false,
-        
-    // }
     
     handleClick() {
         console.log('click happened')
@@ -53,10 +43,7 @@ class UnitOverlay extends Component {
     render() {
         return (
         <div className='unitOverlay'>
-            <ImageMapper src={unit} width={1440} imgWidth={1920} map={MAP} onClick={()=> this.handleClick()}/>
-            <Button variant="primary" onClick={this.handleShow}>
-          Launch modal
-        </Button>
+            <ImageMapper src={unit} width={1440} imgWidth={1920} map={MAP} onClick={()=> this.handleShow()}/>
         <Modal show={this.state.show} onHide={this.handleClose}>
                     <Modal.Header closeButton>
                         <Modal.Title>Modal Title</Modal.Title>
