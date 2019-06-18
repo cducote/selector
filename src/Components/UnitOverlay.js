164de2eabd50f9624e32ff2a4b582e5aef80b060
cducote <chrisducote89@gmail.com>
2019-06-18 14:13:16 -0400

    unique modals and handlers

--- src/Components/UnitOverlay.js
+++ src/Components/UnitOverlay.js
@@ -3,9 +3,6 @@ import {Button, Modal, Container, Card } from 'react-bootstrap'
 import unit from '../Images/units/unit.png'
 import ImageMapper from 'react-image-mapper'
 
-
-
-
 let MAP = {
 	id: 'map', name: 'my-map',
 	areas: [
@@ -25,6 +22,13 @@ let MAP = {
 class UnitOverlay extends Component {
         state = {
           areaClicked: 0,
+          showModalBF: false,
+          showModalLF: false,
+          showModalBalcony: false,
+          showModalCloset: false,
+          showModalHall: false,
+          showModalShower: false,
+          showModalVanity: false,
           showModalEntry: false,
           showModalKitchen: false,
           showModalBP: false,
@@ -46,8 +50,22 @@ class UnitOverlay extends Component {
     
     determineModal() {
         let areaId = this.state.areaClicked
-        if (areaId === 7){
+        if (areaId === 1){
+            this.handleShowModalBF()
+        } else if (areaId === 2){
+            this.handleShowModalLF()
+        } else if (areaId === 3){
+            this.handleShowModalBalcony()
+        } else if (areaId === 4){
+            this.handleShowModalCloset()
+        } else if (areaId === 5){
+            this.handleShowModalHall()
+        } else if (areaId === 6){
+            this.handleShowModalShower()
+        } else if (areaId === 7){
             this.handleShowModalKitchen()
+        } else if (areaId === 8){
+            this.handleShowModalVanity()
         } else if (areaId === 9){
             this.handleShowModalBP()
         } else if (areaId === 10) {
@@ -56,16 +74,57 @@ class UnitOverlay extends Component {
         console.log(areaId)
     }    
     areaCheck = async (area) => {
-        // console.log(area.id)
         await this.setState({ areaClicked: area.id })
         this.determineModal()
     }
     
+    handleCloseModalBF = async () =>{
+        this.setState({ showModalBF: false });
+      }
+    handleShowModalBF = async () => {
+        this.setState({ showModalBF: true });
+      }
+    handleCloseModalLF = async () =>{
+        this.setState({ showModalLF: false });
+      }
+    handleShowModalLF = async () => {
+        this.setState({ showModalBP: true });
+      }
+    handleCloseModalBalcony = async () =>{
+        this.setState({ showModalBalcony: false });
+      }
+    handleShowModalBalcony = async () => {
+        this.setState({ showModalBalcony: true });
+      }
+    handleCloseModalCloset = async () =>{
+        this.setState({ showModalCloset: false });
+      }
+    handleShowModalCloset = async () => {
+        this.setState({ showModalCloset: true });
+      }
+    handleCloseModalHall = async () =>{
+        this.setState({ showModalHall: false });
+      }
+    handleShowModalHall = async () => {
+        this.setState({ showModalHall: true });
+      }
+    handleCloseModalShower = async () =>{
+        this.setState({ showModalShower: false });
+      }
+    handleShowModalShower = async () => {
+        this.setState({ showModalShower: true });
+      }
+    handleCloseModalVanity = async () =>{
+        this.setState({ showModalVanity: false });
+      }
+    handleShowModalVanity = async () => {
+        this.setState({ showModalVanity: true });
+      }
     handleCloseModalEntry = async () => {
         this.setState({ showModalEntry: false });
       }
     handleShowModalEntry = async () => {
-            this.setState({ showModalEntry: true });
+        this.setState({ showModalEntry: true });
         }
     handleCloseModalKitchen = async () => {
         this.setState({ showModalKitchen: false });
@@ -73,7 +132,7 @@ class UnitOverlay extends Component {
     handleShowModalKitchen = async () => {
         this.setState({ showModalKitchen: true });
       }
-      handleCloseModalBP = async () =>{
+    handleCloseModalBP = async () =>{
         this.setState({ showModalBP: false });
       }
     handleShowModalBP = async () => {
@@ -86,21 +145,203 @@ class UnitOverlay extends Component {
             <ImageMapper src={unit} width={1440} imgWidth={1920} map={MAP} 
                         onClick={(area)=> this.areaCheck(area)} 
                         />
-        {/* Entry Modal */}
-        <Modal show={this.state.showModalEntry} onHide={this.handleCloseModalEntry}>
+        {/* Bed Fan */}
+        <Modal show={this.state.showModalBF} onHide={this.handleCloseModalBF}>
                     <Modal.Header closeButton>
-                        <Modal.Title>Selection</Modal.Title>
+                        <Modal.Title>Bed Room</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                            <Card>
+                                <Card.Header>
+                                    Choose a fan by clicking below 
+                                </Card.Header>
+                                    <Card.Body>
+                                        <img alt='test' src={this.state.lights["11L300701 WH"].image}/>
+                                    </Card.Body>
+                            </Card>
+                        </Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModalBF}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleShowModalBF}>
+                            Save Changes
+                        </Button>
+                        </Modal.Footer>
+                </Modal>
+        {/* Living Room Fan */}
+        <Modal show={this.state.showModalLF} onHide={this.handleCloseModalLF}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Living Room</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <Container>
                                     <Card>
                                        <Card.Header>
-                                            Entry
+                                       Choose a fan by clicking below
                                         </Card.Header>
                                             <Card.Body>
                                                 <img alt='test' src={this.state.lights["11L300701 WH"].image}/>
                                             </Card.Body>
                                     </Card>
+                        </Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModalLF}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleShowModalLF}>
+                            Save Changes
+                        </Button>
+                        </Modal.Footer>
+                </Modal>
+        {/* Balcony Modal */}
+        <Modal show={this.state.showModalBalcony} onHide={this.handleCloseModalBalcony}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Balcony</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                            <Card>
+                                <Card.Header>
+                                    Select a light
+                                </Card.Header>
+                                    <Card.Body>
+                                        <img alt='test' src={this.state.lights["11L300701 WH"].image}/>
+                                    </Card.Body>
+                            </Card>
+                        </Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModalBalcony}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleShowModalBalcony}>
+                            Save Changes
+                        </Button>
+                        </Modal.Footer>
+                </Modal>
+        {/* Closet */}
+        <Modal show={this.state.showModalCloset} onHide={this.handleCloseModalCloset}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Closet</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                            <Card>
+                                <Card.Header>
+                                    select a light
+                                </Card.Header>
+                                    <Card.Body>
+                                        <img alt='test' src={this.state.lights["11L300701 WH"].image}/>
+                                    </Card.Body>
+                            </Card>
+                        </Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModalCloset}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleShowModalCloset}>
+                            Save Changes
+                        </Button>
+                        </Modal.Footer>
+                </Modal>
+        {/* Hallway */}
+        <Modal show={this.state.showModalHall} onHide={this.handleCloseModalHall}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Hallway</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                                    <Card>
+                                       <Card.Header>
+                                            select a hallway light
+                                        </Card.Header>
+                                            <Card.Body>
+                                                <img alt='test' src={this.state.lights["11L300701 WH"].image}/>
+                                            </Card.Body>
+                                    </Card>
+                        </Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModalHall}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleShowModalHall}>
+                            Save Changes
+                        </Button>
+                        </Modal.Footer>
+                </Modal>
+        {/* Shower */}
+        <Modal show={this.state.showModalShower} onHide={this.handleCloseModalShower}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Shower</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                            <Card>
+                                <Card.Header>
+                                    select a shower light
+                                </Card.Header>
+                                    <Card.Body>
+                                        <img alt='test' src={this.state.lights["11L300701 WH"].image}/>
+                                    </Card.Body>
+                            </Card>
+                        </Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModalShower}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleShowModalShower}>
+                            Save Changes
+                        </Button>
+                        </Modal.Footer>
+                </Modal>
+        {/* Vanity */}
+        <Modal show={this.state.showModalVanity} onHide={this.handleCloseModalVanity}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Vanity</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                            <Card>
+                                <Card.Header>
+                                    selct a vanity light
+                                </Card.Header>
+                                    <Card.Body>
+                                        <img alt='test' src={this.state.lights["11L300701 WH"].image}/>
+                                    </Card.Body>
+                            </Card>
+                        </Container>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleCloseModalVanity}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleShowModalVanity}>
+                            Save Changes
+                        </Button>
+                        </Modal.Footer>
+                </Modal>    
+        {/* Entry Modal */}
+        <Modal show={this.state.showModalEntry} onHide={this.handleCloseModalEntry}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Entryway</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Container>
+                            <Card>
+                                <Card.Header>
+                                    select and entryway light
+                                </Card.Header>
+                                    <Card.Body>
+                                        <img alt='test' src={this.state.lights["11L300701 WH"].image}/>
+                                    </Card.Body>
+                            </Card>
                         </Container>
                     </Modal.Body>
                     <Modal.Footer>
@@ -116,18 +357,18 @@ class UnitOverlay extends Component {
                 {/* Kitch Modal */}
                 <Modal show={this.state.showModalKitchen} onHide={this.handleCloseModalKitchen}>
                     <Modal.Header closeButton>
-                        <Modal.Title>Selection</Modal.Title>
+                        <Modal.Title>Kitchen</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <Container>
-                                    <Card>
-                                        <Card.Header>
-                                            Kitchen
-                                        </Card.Header>
-                                            <Card.Body>
-                                                <img alt='test' src={this.state.lights["21L303004 BN"].image}/>
-                                            </Card.Body>
-                                    </Card>              
+                            <Card>
+                                <Card.Header>
+                                    Kitchen
+                                </Card.Header>
+                                    <Card.Body>
+                                        <img alt='test' src={this.state.lights["21L303004 BN"].image}/>
+                                    </Card.Body>
+                            </Card>              
                         </Container>
                     </Modal.Body>
                     <Modal.Footer>
