90d47238964672bcae34f4b200871b427c53b23f
cducote <chrisducote89@gmail.com>
2019-07-06 13:56:08 -0400

    qty added to sample data

--- src/API/sampleKitchen.js
+++ src/API/sampleKitchen.js
@@ -3,22 +3,26 @@ let kitchen =
         {
             "partnumber": "21L303004 BN",
             "image": require("../Images/lights/21L303004 BN.jpg"),
-            "retailPrice": "0.00"
+            "retailPrice": "0.00",
+            "qty": '0'
           },
           {
             "partnumber": "212704 BN",
             "image": require("../Images/lights/212704 BN.jpg"),
-            "retailPrice": "0.00"
+            "retailPrice": "0.00",
+            "qty": '0'
           },
           {
             "partnumber": "213404 BN",
             "image": require("../Images/lights/213404 BN.jpg"),
-            "retailPrice": "0.00"
+            "retailPrice": "0.00",
+            "qty": '0'
           },
           {
             "partnumber": "22L354803 WH",
             "image": require("../Images/lights/22L354803 WH.jpg"),
-            "retailPrice": "0.00"
+            "retailPrice": "0.00",
+            "qty": '0'
           }
 
     ]
