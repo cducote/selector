19b1f8a957322a245b9bdec738e67a3e6f86d439
cducote <chrisducote89@gmail.com>
2019-08-26 15:45:17 -0400

    cooridor mapped

--- src/Components/CImageMap.js
+++ src/Components/CImageMap.js
@@ -1,8 +1,8 @@
 let CORMAP = {
     id: 'map2', name: 'cooridor-map',
     areas: [
-        {id: 1, name: "shower", shape: 'rect', coords: [40,838,282,1077], preFillColor: 'clear'},
-        {id: 2, name: "vanity", shape: 'rect', coords: [40,554,286,801], preFillColor: 'clear'},
-        {id: 3, name: "closet", shape: 'rect', coords: [37,277,284,522], preFillColor: 'clear'}]}
+        {id: 1, name: "shower", shape: 'rect', coords: [1487,775,1729,1017], preFillColor: 'clear'},
+        {id: 2, name: "vanity", shape: 'rect', coords: [527,773,769,1017], preFillColor: 'clear'},
+        {id: 3, name: "closet", shape: 'rect', coords: [895,25,1139,271], preFillColor: 'clear'}]}
 
 export default CORMAP
--- src/Components/Cooridor.js
+++ src/Components/Cooridor.js
@@ -1,44 +1,43 @@
 import React, { Component } from 'react';
 import {Container} from "react-bootstrap";
-// import ImageMapper from "react-image-mapper";
-// import entryLights from '../API/sampleEntry'
-// import outdoorLights from '../API/sampleBalcony'
-// import CImageMap from './CImageMap'
-// import cooridorImg from '../Images/units/CorridorNoEM'
+import ImageMapper from "react-image-mapper";
+import entryLights from '../API/sampleEntry'
+import outdoorLights from '../API/sampleBalcony'
+import CImageMap from './CImageMap'
+import CorridorNoEM from '../Images/corridor/CorridorNoEM.png'
 
-// let CORMAP = CImageMap
+let CORMAP = CImageMap
 
 class Cooridor extends Component {
     render() {
 
-    // let small = 375;
-    // let medium = 750;
-    // let large = 1024
-    // let responsive = 400
-    // let width = window.innerWidth
-    // if (width >= 900) {
-    //   responsive = large
-    // } else if (width >= 700){
-    //   responsive = medium
-    // } else {
-    //   responsive = small
-    // }
-    // let responsiveUnitMapper = (
-    //   <ImageMapper
-    //         src={cooridorImg}
-    //         width={responsive}
-    //         imgWidth={1920}
-    //         map={CORMAP}
-    //         onClick={area => this.areaCheck(area)}
-    //       />
-    // )
+    let small = 375;
+    let medium = 750;
+    let large = 1024
+    let responsive = 400
+    let width = window.innerWidth
+    if (width >= 900) {
+      responsive = large
+    } else if (width >= 700){
+      responsive = medium
+    } else {
+      responsive = small
+    }
+    let responsiveUnitMapper = (
+      <ImageMapper
+            src={CorridorNoEM}
+            width={responsive}
+            imgWidth={1920}
+            map={CORMAP}
+            onClick={area => this.areaCheck(area)}
+          />
+    )
 
         return (
             <>
                 <Container fluid className="unitContainer">
                      <div>
-                        {/* {responsiveUnitMapper} */}
-                        FUCK YOU 
+                        {responsiveUnitMapper}
                      </div>
                     
                 </Container>
